82c03d12a8dd6bbbf64b18f7b7f852629314ccef
K Briaan <kbriaan1@gmail.com>
2024-07-31 22:24:08 +0300

    SERVER CONNECTED

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.connect("mongodb+srv://kbriaan1:chemutai@cluster0.fnvjzai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("DB Connection successful")).catch((err)=>{console.log(err)})
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connection successful")).catch((err)=>{console.log(err)})
 
 app.listen(5000, ()=>{
     console.log("Backend server is running...")
