96a3a563abee0d7b981096346bf0e28845583dec
K Briaan <kbriaan1@gmail.com>
2024-07-31 22:21:04 +0300

    DB CONNECTION

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
 
 mongoose.connect("mongodb+srv://kbriaan1:chemutai@cluster0.fnvjzai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("DB Connection successful")).catch((err)=>{console.log(err)})
 
