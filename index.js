1abdc0c155976b8bce539ede5ad6f9ad0687cbc6
K Briaan <kbriaan1@gmail.com>
2024-07-31 22:02:04 +0300

    error

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
 
+mongoose.connect("mongodb+srv://kbriaan1:chemutai@cluster0.fnvjzai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("DB Connection successful")).catch((err)=>{console.log(err)})
+
 app.listen(5000, ()=>{
     console.log("Backend server is running...")
 });
