4a026e5dee9e206e87e74eab98278028ba873f0c
K Briaan <kbriaan1@gmail.com>
2024-07-31 21:45:44 +0300

    backend setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello 1")
+const express = require("express")
+const app = express();
+const mongoose = require("mongoose")
+
+app.listen(5000, ()=>{
+    console.log("Backend server is running...")
+});
