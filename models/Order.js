a9b3da16689d0d85646c671fcd27155df86a96b1
K Briaan <kbriaan1@gmail.com>
2024-08-01 13:13:39 +0300

    done with order.js

--- models/Order.js
+++ models/Order.js
@@ -1,26 +1,27 @@
 const mongoose = require("mongoose");
 
-const CartSchema = new mongoose.Schema(
+const OrderSchema = new mongoose.Schema(
   {
     userId: {
       type: String,
       required: true,
     },
     products: [
-{
-    productId:{
-        type:String
-    },
-    quantity:{
-        quantity:Number,
-        default:1
-    }
-}
-    ]
-    
-    
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          quantity: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount:{type:Number, required:true},
+    address:{type:Object, required:true},
+    status:{type:String, default:"pending"}
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Cart", CartSchema);
+module.exports = mongoose.model("Order", OrderSchema);
