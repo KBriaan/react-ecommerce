1b0ee553afc4e22f31bae176407715fd771d4ccc
K Briaan <kbriaan1@gmail.com>
2024-08-01 13:20:13 +0300

    route files

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,6 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res)=>{
-    res.send("user test is successful")
-});
 
-router.post("/usersposttest", (req, res)=>{
-    const username = req.body.username;
-    console.log("your username is:" +username);
-});
 
 
 module.exports= router
